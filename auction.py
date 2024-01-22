b564157de8f05f79aab570cf612e75c6ebeef1dc
giladosk <gilad@metomotion.com>
2024-01-22 22:24:04 +0200

    fixed desired_good logic

--- auction.py
+++ auction.py
@@ -11,21 +11,21 @@ print(f'{cost_matrix}')
 num_bidders = cost_matrix.shape[0]
 num_goods = cost_matrix.shape[1]
 
-owners_allocated_to_goods = {key: {'owner': key, 'best_price': 0} for key in range(num_goods)}
+owners_allocated_to_goods = {key: key for key in range(num_goods)}
+best_prices = [0] * num_bidders
 bidders_queue = list(range(num_bidders))
-# delta_price = 1 / num_bidders + 1
-delta_price = 0
+epsilon_price = 1 / num_bidders + 1
 
 while len(bidders_queue) > 0:
     bidder = bidders_queue.pop(0)  # take the first bidder in line
     print(f'{bidder=}')
-    desired_good = np.argmax(cost_matrix[bidder, :])
-    price_rise = cost_matrix[bidder, desired_good] - owners_allocated_to_goods[desired_good]['best_price']
-    if price_rise > 0:
+    desired_good = np.argmax(benefits := (cost_matrix[bidder, :] - best_prices))
+    price_rise = benefits[desired_good]
+    if price_rise > epsilon_price and bidder != owners_allocated_to_goods[desired_good]:
         # put previous bidder in end of queue, and set new bidder as the owner
-        bidders_queue.append(owners_allocated_to_goods[desired_good]['owner'])
-        owners_allocated_to_goods[desired_good]['owner'] = bidder
-        owners_allocated_to_goods[desired_good]['best_price'] += price_rise + delta_price
-    print(owners_allocated_to_goods)
+        bidders_queue.append(owners_allocated_to_goods[desired_good])
+        owners_allocated_to_goods[desired_good] = bidder
+        best_prices[desired_good] += epsilon_price
+    print([f'{x[0]}: {x[1]}, {y}' for x, y in zip(owners_allocated_to_goods.items(), best_prices)])
 
 last_line = 0

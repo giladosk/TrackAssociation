e0eb750a70c61b6c24bc751838b7f0e1f4203ee3
giladosk <gilad@metomotion.com>
2024-01-24 15:54:22 +0200

    added final results logging

--- auction.py
+++ auction.py
@@ -1,10 +1,15 @@
 import numpy as np
 
-cost_matrix_minimise = np.array([[108, 125, 150],
-                                 [150, 135, 175],
-                                 [122, 148, 250]])  # we know the min assignments are: (0, 2), (1, 1), (2, 0)
-
-cost_matrix = np.amax(cost_matrix_minimise) - cost_matrix_minimise
+# cost_matrix_minimise = np.array([[108, 125, 150],
+#                                  [150, 135, 175],
+#                                  [122, 148, 250]])  # we know the min assignments are: (0, 2), (1, 1), (2, 0)
+#
+# cost_matrix = np.amax(cost_matrix_minimise) - cost_matrix_minimise
+cost_matrix = np.array([[2, 4, 3, 5, 4],
+                        [7, 4, 6, 8, 4],
+                        [2, 9, 8, 10, 4],
+                        [8, 6, 12, 7, 4],
+                        [2, 8, 5, 8, 8]])
 print(f'{cost_matrix}')
 
 # rows = bidders (or owners), columns = goods
@@ -14,7 +19,8 @@ num_goods = cost_matrix.shape[1]
 owners_allocated_to_goods = {key: key for key in range(num_goods)}
 best_prices = [0] * num_bidders
 bidders_queue = list(range(num_bidders))
-epsilon_price = 1 / num_bidders + 1
+epsilon_price = round(np.log10(np.mean(cost_matrix)))
+# epsilon_price = 1 / num_bidders + 1
 
 while len(bidders_queue) > 0:
     bidder = bidders_queue.pop(0)  # take the first bidder in line
@@ -27,5 +33,11 @@ while len(bidders_queue) > 0:
         owners_allocated_to_goods[desired_good] = bidder
         best_prices[desired_good] += epsilon_price
     print([f'{x[0]}: {x[1]}, {y}' for x, y in zip(owners_allocated_to_goods.items(), best_prices)])
+
+total_price = 0
+for item, owner in owners_allocated_to_goods.items():
+    print(f'{owner=}: {item=} {cost_matrix[owner, item]=}')
+    total_price += cost_matrix[owner, item]
+print(f'{total_price=}')
 
 last_line = 0
